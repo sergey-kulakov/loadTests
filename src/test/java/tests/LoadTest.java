e9a6d6d42cb7023af9f06fd77789278db0de493d
Sergey <sergey.kulakov@inspirativ.com>
2017-05-25 16:04:51 +0300

    Simple test completed

---
+++ src/test/java/tests/LoadTest.java
@@ -0,0 +1,40 @@
+package tests;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class LoadTest {
+    public static WebDriver driver;
+
+    @Test
+    public void searchTest() throws InterruptedException {
+
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+
+        driver.get("https://vigbo.com/");
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        // Получаем время Load Event End (окончание загрузки страниы)
+        long loadEventEnd = (Long) js.executeScript("return window.performance.timing.loadEventEnd;");
+        // Получаем Navigation Event Start (начало перехода)
+        long navigationStart = (Long) js.executeScript("return window.performance.timing.navigationStart;");
+        // Разница между Load Event End и Navigation Event Start - это время загрузки страницы
+        System.out.println("Page Load Time is " + (loadEventEnd - navigationStart)/1000 + " seconds.");
+
+
+    }
+
+    @AfterTest
+    public void brorserQuit() {
+        driver.quit();
+    }
+
+
+}
