96377834d641c3aafacf85f7037a7e68bd39a81b
Sergey <sergey.kulakov@inspirativ.com>
2017-05-30 12:52:01 +0300

    Run from suite

--- src/test/java/tests/LoadTest.java
+++ src/test/java/tests/LoadTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
@@ -11,15 +12,22 @@ import java.util.concurrent.TimeUnit;
 public class LoadTest {
     public static WebDriver driver;
 
+
+
+
+
+
     @Test
-    public void searchTest() throws InterruptedException {
+    @Parameters({"url"})
+
+        public void searchTest(String url) throws InterruptedException {
 
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         driver.manage().window().maximize();
 
-        driver.get("https://vigbo.com/");
+        driver.get(url);
         JavascriptExecutor js = (JavascriptExecutor) driver;
         // Получаем время Load Event End (окончание загрузки страниы)
         long loadEventEnd = (Long) js.executeScript("return window.performance.timing.loadEventEnd;");
